75a89e765e57325eda09582ca325cb62702a015e
Vinicius Marinho <48867230+ViniciusAlexsander@users.noreply.github.com>
2022-06-17 12:13:41 -0300

    listando e validando criação de categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -7,9 +7,21 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
+  const categoriesAlreadyExist = categoriesRepository.findByName(name);
+
+  if (categoriesAlreadyExist) {
+    return res.status(400).json({ error: "Category Already exists" });
+  }
+
   categoriesRepository.create({ name, description });
 
   return res.status(201).send();
+});
+
+categoriesRoutes.get("/", (req, res) => {
+  const categories = categoriesRepository.list();
+
+  return res.status(200).json(categories);
 });
 
 export { categoriesRoutes };
