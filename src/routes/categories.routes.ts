ab1fcd9280107df457a871511cddf09409cab4fe
Vinicius Marinho <48867230+ViniciusAlexsander@users.noreply.github.com>
2022-07-07 21:27:51 -0300

    criando rota de importacao de categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
+import multer from "multer";
+
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (req, res) => {
   return createCategoryController.handle(req, res);
@@ -12,6 +19,10 @@ categoriesRoutes.get("/", (req, res) => {
   const categories = listCategoriesController.handle(req, res);
 
   return res.status(200).json(categories);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
+  return importCategoryController.handle(req, res);
 });
 
 export { categoriesRoutes };
