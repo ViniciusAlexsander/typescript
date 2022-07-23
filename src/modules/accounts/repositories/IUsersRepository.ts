dbfbb8d1352f4f2623fa3298311ef4232ee60e22
Vinicius Marinho <48867230+ViniciusAlexsander@users.noreply.github.com>
2022-07-23 00:43:59 -0300

    criando token de user

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -4,6 +4,7 @@ import { User } from "../entities/User";
 interface IUsersRepository {
   create(user: ICreateUserDTO): Promise<void>;
   findByEmailOrDriver(email: string, driverLicense: string): Promise<User>;
+  findByEmail(email: string): Promise<User>;
 }
 
 export { IUsersRepository };
