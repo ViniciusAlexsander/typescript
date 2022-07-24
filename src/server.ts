9a955f4877a6beda26da87ebfb9a6be5cf7441cf
Vinicius Marinho <48867230+ViniciusAlexsander@users.noreply.github.com>
2022-07-24 14:47:53 -0300

    alterando documentacao

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
